9972a9afc5378df49a20cee1c79246931a61df55
Genevieve Costa <togenny@yahoo.com>
2017-10-02 22:53:20 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,25 @@
-# your code goes here
+def begins_with_r(array)
+  array.all? do |name| name.start_with?("r")
+end
+end
+
+def contain_a(array)
+  array.select do |name| name.include?("a")
+  end
+end
+
+def first_wa(array)
+  array.find do |word|
+    if word[0, 2] == "wa"
+      return word
+  end
+end
+end
+
+def remove_non_strings(array)
+ array.delete_if {|name| name.class != String}
+ return array
+end
+
+def count_elements
+  array.group_by{}
