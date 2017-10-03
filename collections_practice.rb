889530e99f95efc2d495e206ad3a8546e6679dbd
Genevieve Costa <togenny@yahoo.com>
2017-10-03 00:23:17 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -21,5 +21,41 @@ def remove_non_strings(array)
  return array
 end
 
-def count_elements
-  array.group_by{}
+def count_elements(array)
+  array.each do |first_hash|
+    first_hash[:count] = 0
+    name = first_hash[:name]
+    array.each do |hash|
+      if hash[:name] == name
+        first_hash[:count] += 1
+      end
+    end
+  end.uniq
+end
+
+def merge_data(keys, data)
+  keys.each do |name|
+    data.each do |hash|
+      name.merge!(hash[name[:first_name]])
+    end
+  end
+end
+
+
+def find_cool(array)
+  array.select {|value| value if value.has_value?("cool")}
+end
+
+
+def organize_schools(array)
+  new_hash = Hash.new
+
+  array.each do |key, value|
+      if new_hash.include?(value[:location])
+        new_hash[value[:location]] << key
+      else
+        new_hash[value[:location]] = [key]
+      end
+  end
+  new_hash
+end
